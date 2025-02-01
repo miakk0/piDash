da1aa9c7c2ffa644f7f8fceed026fcb1d7a7b783
Tyann Ly <tyannly2002@gmail.com>
2025-02-01 16:32:35 -0500

    Integrating Weather info API and cleaning up weather_info.py

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,11 +1,11 @@
 from flask import Flask
-
+import weather_info
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
+@app.route('/current_weather')
+def get_current_weather():
+    current, forecast = weather_info.get_weather()
+    return {"current_weather": current, "forecast": forecast}
 
 if  __name__ == '__main__':
     app.run(debug=True)
--- flask-server/weather_info.py
+++ flask-server/weather_info.py
@@ -1,15 +1,15 @@
 import openmeteo_requests
-import os
-import datetime
+from datetime import datetime
+from dateutil import tz
 import requests_cache
-import pandas as pd
 from retry_requests import retry
 
-# Make sure all required weather variables are listed here
-# The order of variables in hourly or daily is important to assign them correctly below
+def generate_list_of_datetime(start: int, end: int, interval: int, timezone:str):
+    datetime_list = [datetime.fromtimestamp(timestamp, tz=tz.gettz(timezone)) for timestamp in range(start, end, interval)]
+    return datetime_list
+
+
 def get_weather():
-    #if os.path.exists("weather.txt"): 
-    #    os.remove("weather.txt")
     # Setup the Open-Meteo API client with cache and retry on error
     cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
     retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
@@ -20,7 +20,7 @@ def get_weather():
         "longitude": -73.5878,
         "current": ["temperature_2m", "apparent_temperature", "rain", "showers", "snowfall", "weather_code", "cloud_cover"],
         "hourly": ["temperature_2m", "apparent_temperature", "weather_code", "cloud_cover"],
-        "timezone": "America/New_York",
+        "timezone": "America/Montreal",
         "forecast_days": 1,
         "models": ["gfs_hrrr"]
     }
@@ -29,68 +29,16 @@ def get_weather():
     # Process first location. Add a for-loop for multiple locations or weather models
     for i in range(len(params["models"])):
         response = responses[i]
-        print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
-        print(f"Elevation {response.Elevation()} m asl")
-        print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
-        print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
-
 
         # Current values. The order of variables needs to be the same as requested.
         current = response.Current()
-        current_temperature_2m = current.Variables(0).Value()
-        current_apparent_temperature = current.Variables(1).Value()
-        current_rain = current.Variables(2).Value()
-        current_showers = current.Variables(3).Value()
-        current_snowfall = current.Variables(4).Value()
-        current_weather_code = current.Variables(5).Value()
-        current_cloud_cover = current.Variables(6).Value()
+        current_data = {params["current"][i]: current.Variables(i).Value() for i in range(len(params["current"]))}
 
-        variables = {
-            "current_temperature_2m": round(current_temperature_2m,2),
-            "current_apparent_temperature": current_apparent_temperature,
-            "current_rain": current_rain,
-            "current_showers": current_showers,
-            "current_snowfall": current_snowfall,
-            "current_weather_code": current_weather_code,
-            "current_cloud_cover": current_cloud_cover,
-        }
-
-        print(f"Current time {current.Time()}")
-
-        print(f"Current temperature_2m {current_temperature_2m}")
-        print(f"Current apparent_temperature {current_apparent_temperature}")
-        print(f"Current rain {current_rain}")
-        print(f"Current showers {current_showers}")
-        print(f"Current snowfall {current_snowfall}")
-        print(f"Current weather_code {current_weather_code}")
-        print(f"Current cloud_cover {current_cloud_cover}")
-
-        # Process hourly data. The order of variables needs to be the same as requested.
         hourly = response.Hourly()
-        hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
-        hourly_apparent_temperature = hourly.Variables(1).ValuesAsNumpy()
-        hourly_weather_code = hourly.Variables(2).ValuesAsNumpy()
-        hourly_cloud_cover = hourly.Variables(3).ValuesAsNumpy()
+        hourly_data = {"date": generate_list_of_datetime(hourly.Time(), hourly.TimeEnd(), hourly.Interval(), str(response.Timezone()))}
+        for i in range(len(params["hourly"])):
+            hourly_data[params["hourly"][i]] = hourly.Variables(i).ValuesAsNumpy().tolist()
 
-        hourly_data = {"date": pd.date_range(
-            start = pd.to_datetime(hourly.Time(), unit = "s", utc = True),
-            end = pd.to_datetime(hourly.TimeEnd(), unit = "s", utc = True),
-            freq = pd.Timedelta(seconds = hourly.Interval()),
-            inclusive = "left"
-        )}
 
-        hourly_data["temperature_2m"] = hourly_temperature_2m
-        hourly_data["apparent_temperature"] = hourly_apparent_temperature
-        hourly_data["weather_code"] = hourly_weather_code
-        hourly_data["cloud_cover"] = hourly_cloud_cover
-
-        #hourly_dataframe = pd.DataFrame(data = hourly_data)
-        #with open("weather.txt", 'a') as f:
-        #    now = datetime.datetime.now()
-        #    f.write(f"Recorded at: {now} using {params['models'][i]}\n")
-        #    for name, value in variables:
-        #        f.write(f"{name}: {value}\n")
-        #    df_string = hourly_dataframe.to_string(header=True, index=True)
-        #    f.write(df_string + "\n\n\n")
-    return variables
+    return current_data, hourly_data
 
