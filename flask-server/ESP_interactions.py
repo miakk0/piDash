0f61f203ea4c953a8def7dc024520defb03e8c05
Tyann Ly <tyannly2002@gmail.com>
2025-02-02 13:19:35 -0500

    Changed from Serial communication to REST

--- flask-server/ESP_interactions.py
+++ flask-server/ESP_interactions.py
@@ -1,16 +1,7 @@
-import serial
+import requests
 
-def read_sensor_data(esp_port):
-    baud_rate = 9600
-    """Read temperature and humidity from ESP8266 and update the GUI."""
-    try:
-        with serial.Serial(esp_port, baud_rate, timeout=1) as ser:
-            while True:
-                raw_data = ser.readline().decode('utf-8').strip()
-                if raw_data:
-                    data = raw_data.split()
-                    return {"humidity": data[0], "temperature": data[1]}
-    except serial.SerialException:
-        return f"Invalid serial port name: {esp_port}"
-    except UnicodeDecodeError:
-        return f"Error reading from serial port: {esp_port}"
+# Fetch data
+def read_sensor_data(esp_ip):
+
+    data = requests.get(f"http://{esp_ip}/DHT11").json()
+    return data
